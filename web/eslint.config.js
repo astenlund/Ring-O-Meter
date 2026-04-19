be1e1f39e45be09be0f380ae356e827d84580fe8
Andreas Stenlund <a.stenlund@gmail.com>
2026-04-19 17:07:15 +0200

    chore(web): add ESLint flat config and lint scripts

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,23 +1,72 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+const sharedRules = {
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single', {avoidEscape: true, allowTemplateLiterals: true}],
+    'no-trailing-spaces': 'error',
+    'eol-last': ['error', 'always'],
+    'indent': ['error', 4, {SwitchCase: 1}],
+    'no-multiple-empty-lines': ['error', {max: 1, maxEOF: 0}],
+    'no-var': 'error',
+    'prefer-const': 'error',
+
+    'eqeqeq': ['error', 'always', {null: 'ignore'}],
+    'no-duplicate-case': 'error',
+    'no-dupe-keys': 'error',
+    'no-unreachable': 'error',
+};
+
+export default [
+    {
+        ignores: ['dist/**', 'node_modules/**', '.vite/**'],
     },
-  },
-])
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ['src/**/*.{ts,tsx}'],
+        plugins: {
+            import: importPlugin,
+            react,
+            'react-hooks': reactHooks,
+        },
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parserOptions: {
+                ecmaFeatures: {jsx: true},
+            },
+            globals: {
+                window: 'readonly',
+                document: 'readonly',
+                console: 'readonly',
+                navigator: 'readonly',
+                AudioContext: 'readonly',
+                AudioWorkletNode: 'readonly',
+                AudioWorkletProcessor: 'readonly',
+                MediaStream: 'readonly',
+                MediaStreamConstraints: 'readonly',
+                MediaDeviceInfo: 'readonly',
+                requestAnimationFrame: 'readonly',
+                cancelAnimationFrame: 'readonly',
+                setTimeout: 'readonly',
+                clearTimeout: 'readonly',
+                performance: 'readonly',
+                self: 'readonly',
+                registerProcessor: 'readonly',
+            },
+        },
+        settings: {react: {version: 'detect'}},
+        rules: {
+            ...sharedRules,
+            'react/jsx-uses-react': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            '@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_'}],
+        },
+    },
+];
